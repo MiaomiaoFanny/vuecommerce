91759b453c330e823091cf036e95c4a74902fc44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-08 07:09:11 +0000

    Deploy hoanguyen311/vuecommerce to github.com/hoanguyen311/vuecommerce.git:gh-pages

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="category">
-    {{ name }}
+      <h1>{{ name }}</h1>
+      <p>Auto Cancellation allows you to only run builds for the latest commits in the queue. This setting can be applied to builds for Branch builds and Pull Request builds separately. Builds will only be canceled if they are waiting to run, allowing for any running jobs to finish.</p>
   </div>
 </template>
 <style>
